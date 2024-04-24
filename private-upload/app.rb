8666284d4ca015a51a0e3dc3cfe307146c8a4682
Nick Brandaleone <nbrand@mac.com>
2024-04-24 13:57:18 -0400

    Working private-upload function

--- private-upload/app.rb
+++ private-upload/app.rb
@@ -9,45 +9,46 @@
 # This function uses private scanning, for unknown files.
 
 require "functions_framework"
+require "google/cloud/storage"
 require "google/cloud/secret_manager"
 
-require "addressable"   # https://github.com/sporkmonger/addressable
+#require 'uri'
 require 'net/http'
 require "rest-client"   # https://github.com/rest-client/rest-client
-require "logger"
 require "json"
 require 'dotenv'
 
-#FunctionsFramework.on_startup do
-#  require_relative "lib/md5_helper"
-#end
+# Moving all init code into `on_startup' block
+FunctionsFramework.on_startup do
+  require_relative "lib/md5_helper"
 
-puts('Starting application private-upload ...')
+  puts('Starting application private-upload ...')
 
-# Dotenv - loads environmental variables.
-# Intended for Development only. However, due to Cloud Function lifecycle,
-# which forces a rebuild during every deploy, it can work in Production as well.
-Dotenv.load 
+  # Dotenv - loads environmental variables.
+  # Intended for Development only. However, due to Cloud Function lifecycle,
+  # which forces a rebuild during every deploy, it can work in Production as well.
+  Dotenv.load 
 
-# Any value will make DEBUG true
-DEBUG = ENV["DEBUG"] || false
-$log = Logger.new(STDOUT)       # $log is a global variable
-DEBUG == true ? $log.level = Logger::DEBUG : $log.level = Logger::INFO
+  # Any value will make DEBUG true.
+  DEBUG = ENV["DEBUG"] || false
+  if DEBUG
+    logger.debug!
+    logger.debug "DEBUG is true"  # warn is a Kernel method, shortcut for STDERR
+  else
+    logger.info "DEBUG is false"
+  end
+end
 
 # Get VirusTotal API key securely via Google Secrets Manager
-def get_secret_apikey
+def get_secret_apikey()
   # Env.fetch("MY_VAR") raises exception if no ENV variable exists
   project_id = ENV["project_id"]
   secret_id  = ENV["secret_id"]
   version_id = ENV["version_id"] || "1"
   
   if project_id.nil? || secret_id.nil?
-    $log.debug "ERROR: environmental variables are not set properly. Terminating..."
+    logger.info "ERROR: environmental variables are not set properly. Terminating..."
     exit(1)
-  end
-  if DEBUG
-    $log.debug "project_id: #{project_id}"
-    $log.debug "secret_id: #{secret_id}"
   end
 
 	# Create a Secret Manager client.
@@ -64,7 +65,7 @@ def get_secret_apikey
 	begin
 		version = client.access_secret_version name: name
 	rescue
-		$log.debug "ERROR: Can't access Google Secrets Manager. Terminating..."
+		warn "ERROR: Can't access Google Secrets Manager. Terminating..."
 		exit(1)
 	end
 
@@ -72,83 +73,133 @@ def get_secret_apikey
 	version.payload.data
 end
 
-# Call VirusTotal private API, and get report on file, using hash as identifier
-def get_virustotal_report(file_hash)
-	base = Addressable::URI.parse("https://www.virustotal.com/api/v3/private/files/")
-	uri = base + file_hash
-	apikey = get_secret_apikey
-	headers = {Accept: 'application/json', 'x-apikey': apikey}
-  
-  http = NET::HTTP.new(uri)
-  
-  request = NET::HTTP::Post.new(uri)
-  request["accept"] = 'application/json'
-  request["content-type"] = 'multipart/form-data; boundary=---011000010111000001101001'
-  request.body = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"disable_sandbox\"\r\n\r\nfalse\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"enable_internet\"\r\n\r\nfalse\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"intercept_tls\"\r\n\r\nfalse\r\n-----011000010111000001101001--"
-  
-  response = http.request(request)
-  puts response.read_body
-  # res = Net::HTTP.start(hostname) do |http|
-  # http.request(req)
-  # end
-  
-  # data = '{"userId": 1, "id": 1, "title": "delectus aut autem", "completed": false}'
-  # http = Net::HTTP.new(hostname)
-  # http.post('/todos', data) do |res|
-  #   p res
-  # end # => #<Net::HTTPCreated 201 Created readbody=true>
-  
-	begin
-	  res = Net::HTTP.get_response(uri, headers)
-	  res.body
-	rescue Exception => error
-		$log.debug "Error connecting to Virus Total."
-    $log.debug error.message
-	end
-end
+# Call VirusTotal private API, and upload a file for scanning
+# https://docs.virustotal.com/reference/post_files
+# File must be less than 32MB. If larger, a different endpoint must be used.
+def private_upload(file)
+  url = "https://www.virustotal.com/api/v3/private/files"
 
-# Parse response fields looking for 'malicous' stats
-def get_score(vt_report)
+  # I should get the secret key once, instead of for every http call
+	apikey = get_secret_apikey()
+	headers = { Accept: 'application/json', 'x-apikey': apikey }
   begin
-    json = JSON.parse(vt_report)
-    score = json['data']['attributes']['last_analysis_stats']['malicious']
-    $log.debug "Malicious score: #{score}"
-    score
-  rescue => e
-    warn "Unable to parse JSON"
-    warn e
-    -1      # flag indicating not able to parse. Most likely unknown to VirusTotal
+      request = RestClient::Request.new({
+        :method => :post,
+        :url => url,
+        :payload => {
+          :multipart => true,
+          :file => File.new(file, 'rb'),
+          :disable_sandbox => 'false',
+          :enable_internet => 'false',
+          :intercept_tls => 'false'
+        }, 
+        :headers => headers})
+       
+      response = request.execute 
+      # Response.body is a string. https://www.rubydoc.info/gems/rest-client/2.1.0/RestClient/Response
+      data_type  = JSON.parse(response.body)["data"]["type"]
+      data_id    = JSON.parse(response.body)["data"]["id"]
+      links_self = JSON.parse(response.body)["data"]["links"]["self"]
+      
+      # Debugger
+      # binding.irb
+      
+  rescue RestClient::ExceptionWithResponse => e
+      warn "Error connecting to Virus Total."
+      warn "HTTP status code #{e.response.code}"
+      warn "Error message: #{e.response.body}"
+  end
+  
+  # Return link to check on scan status
+  links_self
+end
+
+# Check to see if the file has been scanned via VT Private Scanning
+# https://docs.virustotal.com/reference/private-analysis
+# Return true if status is complete. False otherwise.
+def check_vt(url)
+  #url = "https://www.virustotal.com/api/v3/private/analyses/{id}"
+  apikey = get_secret_apikey()
+  headers = { Accept: 'application/json', 'x-apikey': apikey }
+  
+  res = RestClient.get(url, headers)
+  links_self = JSON.parse(res.body)["data"]["links"]["self"]
+  scan_status = JSON.parse(res.body)["data"]["attributes"]["status"]
+  file_sha256 = JSON.parse(res.body)["meta"]["file_info"]["sha256"] rescue nil
+  
+  if DEBUG
+    #logger.debug res
+    puts "check_vt. scan_status is: #{scan_status}"  # will be "queued", "in-progress", "completed"
+    puts "check_vt. SHA256 is: #{file_sha256}"
+  end
+  
+  if scan_status == "completed" 
+    return true, file_sha256
+  else 
+    return false, nil
   end
 end
 
-def list_files
-  if DEBUG
-    p Dir["./*"]
-    p Dir["/bucket/*"]
-    $log.debug "Files: #{Dir["/bucket/*"]}"
+# Get the private scan report. Requires the files SHA-256 as the file ID.
+# https://docs.virustotal.com/reference/private-files-info
+def get_report(sha256)  
+  #url = "https://www.virustotal.com/api/v3/private/files/{id}"
+  url = "https://www.virustotal.com/api/v3/private/files/" + sha256
+  apikey = get_secret_apikey()
+  headers = { Accept: 'application/json', 'x-apikey': apikey }
+  
+  res = RestClient.get(url, headers)
+  verdict = JSON.parse(res.body)["data"]["attributes"]["threat_verdict"] rescue nil
+  
+  verdict
+end
+
+def download_file(bucket, object)
+  project_id = ENV["project_id"]
+  storage = Google::Cloud::Storage.new(
+    project_id: project_id
+  )
+  bucket = storage.bucket bucket
+  file = bucket.file object
+  
+  if false # change to DEBUG if required
+    logger.debug "file is: #{object}"
+    logger.debug "md5 is: #{file.md5()}"
+    logger.debug "size is: #{file.size}"
   end
   
-  # Dir.entries("your/folder").select { |f| File.file? File.join("your/folder", f) }
-  Dir["/bucket/*"]
+  # Copy file from bucket to local filesystem
+  file_path = "/tmp/" + object
+  file.download file_path
+  file_path
 end
 
 FunctionsFramework.http "hello_http" do |request|
-  # FunctionFramework has a global logger object, and local object
+  # FunctionFramework has a global logger object, and local logger object
+  # request.body is an I/O object, not a string. Thus, the rewind method
   message = "I received a request: #{request.request_method} from #{request.url}"
   request.logger.info "#{message}. Body: #{request.body.read}"
-  warn "Testing 1, 2, 3"   # warn is a shortcut
   bucket  = (request.body.rewind && JSON.parse(request.body.read)["bucket"]  rescue nil)
   file    = (request.body.rewind && JSON.parse(request.body.read)["object"]  rescue nil)
-  md5hash = (request.body.rewind && JSON.parse(request.body.read)["md5hash"] rescue nil)
-
-  # TODO: We should validate the input from the EventArc trigger
-  # md5 = get_md5(md5hash)
-  files = list_files()
-  #vt_report = get_virustotal_report(md5)
-  #score = get_score(vt_report)
-  # $log.info "Bucket: #{bucket}, File: #{file}, MD5: #{md5}, Score: #{score}"
-  logger.info "Bucket: #{bucket}, File: #{files}" # Global FF
-
-  #{ 'bucket': bucket, 'object': file, 'score': score.to_s }
-  { 'files': files }
+  
+  # Download file from bucket, so it can be uploaded into VT private scan
+  file_path = download_file(bucket, file)
+  status_link = private_upload(file_path)
+  
+  i = 0   # Loop up to 50 minutes, waiting for scan to be completed
+  while i < 10
+    status, file_sha256 = check_vt(status_link)
+    status ? break : i += 1
+    sleep(5 * 60)  # 5 minutes per loop
+  end
+  
+  # Get scan results
+  # verdict can be: VERDICT_UNDETECTED, 
+  verdict = "Threat Analysis failed"
+  unless file_sha256.nil?
+    verdict = get_report(file_sha256)
+    logger.info verdict
+  end
+  
+  { 'threat_verdict': verdict }
 end
